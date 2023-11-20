863ca2ca7988c38da61f73ffaca2f9d29dccf56c
vedant <ursvedantyetekar@gmail.com>
2023-11-20 15:48:48 +0530

    var, let, const, hoisting, temporal dead zone

---
+++ var-let-const-hoisting-temporal-dead-zone.js
@@ -0,0 +1,9 @@
+console.log(a);
+
+// var a = 10;
+// let a = 10;
+// const a = 10;
+
+// hoisting: it is javascripts default behaviour to move all the declarations to the top.
+// variables declared as var are hoisted and initialised with undefined but the ones declared with let and const are also hoisted but are not initialised.
+// temporal dead zone: A temporal dead zone (TDZ) is the area of a block where a variable is inaccessible until the moment the computer completely initializes it with a value.
